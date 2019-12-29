4f5fd8e6a3cc623eafa27ce589623db5e7b0f7b0
ysk-94 <00ysk.tech@gmail.com>
2019-12-29 15:58:41 +0900

    ch2のコメント追記

--- ch2/inconsistent.js
+++ ch2/inconsistent.js
@@ -17,19 +17,29 @@ function inconsistentRead(filename, callback) {
 
 function createFileReader(filename) {
   const listeners = [];
+  // listenersに登録されている関数をすべて実行するコールバック
+  // を渡して、ファイルを読み込む
   inconsistentRead(filename, value => {
     listeners.forEach(listener => listener(value)); 
   });
 
   return {
+    // onDataReadyに渡された関数はlistenersにpushされ、
+    // fileの読み込みが終わった段階で実行される
     onDataReady: listener => listeners.push(listener),
   };
 }
 
+// data.txtを読み込む
 const reader1 = createFileReader('data.txt');
+// コールバックをlistenerに登録する
 reader1.onDataReady(data => {
   console.log('First call data: ' + data);
+  // すでにdata.txtのキャッシュが存在しているので、コールバックは同期的に実行される
+  // しかしこの時点ではreader2のlistnersは空のため、コールバックは実行されない
   const reader2 = createFileReader('data.txt');
+  // ここでコールバックを登録しても、読み込んだ時点ですでに同期的に実行されてしまっているため、
+  // reader2のlistenersに登録されたコールバックは呼び出されることはない
   reader2.onDataReady( data => {
     console.log('Secont call data: ' + data); 
   });
